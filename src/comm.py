4cb636e620e4c5491d51977fc2a5e39983b14096
LeoCeasar <1304697749@qq.com>
2019-02-08 18:27:13 +0800

    Modified for log.

--- src/comm.py
+++ src/comm.py
@@ -1,10 +1,61 @@
 #!/usr/bin/python3
 # _*_ coding: utf-8 _*_
- 
+
+import os;
+import hashlib
+
+from log.myLog import myLog
+
 class webInfo():
-	url = "";
-	preUrl = "";
-	def __init__(self, url="", pre=""):
-		self.url = url;
-		self.preUrl= pre;
-	 
+    def __init__(self, url="", pre="", depth=1, summary=''):
+        self.url = url;
+        self.preUrl= pre;
+        self.depth = depth;
+        self.summary = summary;
+
+def pathIsExist(path):
+    if os.path.exists(path):
+        print ("file path is exist.")
+        return True;
+    else:
+        print ("file path is not exist.")
+        return False;
+
+def getMd5Code(info):
+    m = hashlib.md5();
+    m.update(info.encode('utf-8'));
+    return m.hexdigest()[8:-8];
+
+def fileReadable(path):
+    if os.access("/file/path/foo.txt", os.R_OK):
+        print ("File is accessible to read")
+    else:
+        print ("File is not accessible to read")
+
+log = None;
+logStatus = False;
+def initLog(logReal, logFile="spider.log", logLevel=5):
+    global log, logStatus;
+    logStatus = logReal
+    if logStatus:
+        log = myLog(logStatus, logFile, logLevel);
+
+def printLog(msg, level):
+    if log is None:
+        print(level + " " + msg);
+    else:
+        log.printLog(msg, level);
+'''
+if logStatus:
+    print("log");
+    def printLog(msg, level):
+        log.printLog(msg, level);
+else :
+    print("print");
+    def printLog(msg, level):
+        print(level + " " + msg);
+'''
+
+if __name__ == "__main__":
+    initLog(True)
+    printLog("test", "ERROR")
